744954c95d09d0c7aac506cb2cf1e3ca9874a025
ttiimmothy <dinosauli2006@gmail.com>
2024-10-01 11:00:58 -0400

    deploy to vercel

--- src/app/(noAuth)/success/page.tsx
+++ src/app/(noAuth)/success/page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
-import TimelineStep from "@/components/order/TimelineStep";
+import TimelineStep from "@/components/order/TimelineStep"
 import React, {useState} from 'react'
 
 const CheckoutSuccess = () => {
